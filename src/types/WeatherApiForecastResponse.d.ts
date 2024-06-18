e44371e267bfa6c1532d7b1d7f6988aa07cb8742
Juston Hart <contact@justonh.art>
2024-06-17 20:53:13 -0500

    Add missing type

---
+++ src/types/WeatherApiForecastResponse.d.ts
@@ -0,0 +1,9 @@
+import WeatherApiCurrent from "./WeatherApiCurrent";
+import WeatherApiForecast from "./WeatherApiForecast";
+import WeatherApiLocation from "./WeatherApiLocation";
+
+export default interface WeatherApiForecastResponse {
+	current: WeatherApiCurrent;
+	forecast: WeatherApiForecast;
+	location: WeatherApiLocation;
+}
